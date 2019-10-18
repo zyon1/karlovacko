5b1370f317907e06a409b85e7c3acc284ee0627f
mariojuk <mariojuk45@gmail.com>
2019-10-18 12:55:59 +0200

    user

--- src/app/events-user/events-user.component.ts
+++ src/app/events-user/events-user.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { ITicket } from "../ticket";
+import { TicketsService } from "../tickets.service";
+import { UserService } from "../user.service";
 
 @Component({
-  selector: 'app-events-user',
-  templateUrl: './events-user.component.html',
-  styleUrls: ['./events-user.component.scss']
+  selector: "app-events-user",
+  templateUrl: "./events-user.component.html",
+  styleUrls: ["./events-user.component.scss"]
 })
 export class EventsUserComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  @Input() ticket: ITicket;
+  tickets$;
+  user$;
+  constructor(
+    private ticketsService: TicketsService,
+    private userService: UserService
+  ) {
+    /*     this.user$ = this.userService.getDoctor$();
+     */
   }
 
+  ngOnInit() {
+    /* this.userService.getDoctor$().subscribe();
+    this.tickets$ = this.ticketsService.getTicket$();
+    this.tickets$.subscribe(console.log); */
+  }
 }
