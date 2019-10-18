75459ce71128e47176a6bfa3d9aca3af45ef09f9
mariojuk <mariojuk45@gmail.com>
2019-10-18 02:00:19 +0200

    price-list

--- src/app/price-list.service.ts
+++ src/app/price-list.service.ts
@@ -1,9 +1,40 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import {
+  AngularFirestore,
+  AngularFirestoreCollection
+} from "@angular/fire/firestore";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class PriceListService {
+  pricesCollection: AngularFirestoreCollection;
+  price$: Observable<any[]>;
+  constructor(private db: AngularFirestore) {
+    this.pricesCollection = this.db.collection<Price[]>("prices");
+    this.price$ = this.pricesCollection.snapshotChanges().pipe(
+      map(actions =>
+        actions.map(a => {
+          const data = a.payload.doc.data() as Price;
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        })
+      )
+    );
+  }
+  addPrice(price: Price) {
+    const ID = this.db.createId();
+    this.pricesCollection.doc(ID).set(price);
+  }
+  getPrice$(id: string) {
+    return this.pricesCollection.doc(id).get();
+  }
+}
 
-  constructor() { }
+export interface Price {
+  name: string;
+  price: number;
+  duration: number;
 }
