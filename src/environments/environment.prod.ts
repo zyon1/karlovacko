8cda1602c8f06f5bb25e52f7ac5bb9f4e4748abc
kdelija <35064609+kdelija@users.noreply.github.com>
2019-10-21 11:07:38 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,5 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "",
-    authDomain: "hackaton-4a2a7.firebaseapp.com",
-    databaseURL: "https://hackaton-4a2a7.firebaseio.com",
-    projectId: "hackaton-4a2a7",
-    storageBucket: "hackaton-4a2a7.appspot.com",
-    messagingSenderId: "331318194791",
-    appId: "1:331318194791:web:354582e52fd57bbf129ac2",
-    measurementId: "G-M0SW8D4S1B"
-  },
+  firebase: {},
   mapsApiKey: ""
 };
